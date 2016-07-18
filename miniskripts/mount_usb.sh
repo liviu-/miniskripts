ab47aba610c42912ebb44539db381fa0a7274c0f
liviu- <lp752@york.ac.uk>
2016-07-18 22:35:03 +0100

    Extend regex rule

--- miniskripts/mount_usb.sh
+++ miniskripts/mount_usb.sh
@@ -2,4 +2,4 @@
 
 # I never really got around setting up auto-mounting so this is
 # a one liner that mounts a SCSI disk when plugged in
-sudo mount $(echo "/dev/$(dmesg | tail | grep -oe "sd[a-c][0-9]")") /mnt
+sudo mount $(echo "/dev/$(dmesg | tail | grep -oe "sd[a-z][0-9]")") /mnt
